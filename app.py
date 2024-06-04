02bc70fbee00dd22c032b2b6686736a12fe060bb
Arun Kumar N <arunkumarn1107@gmail.com>
2024-06-04 20:11:28 +0530

    changes

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def handle_connect():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, host="0.0.0.0", port=5001)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5001,allow_unsafe_werkzeug=True)
